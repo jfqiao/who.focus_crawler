b745649fc88cfecce5132bd2c008f7b1aa552849
jfqiao <jfqiao.123@qq.com>
2018-04-12 13:04:42 +0800

    服务器可以运行爬虫版本

--- run_timer.py
+++ run_timer.py
@@ -6,16 +6,18 @@ import time
 
 def timer():
     cmd = "python /home/jfqiao/project/who.focus_crawler/timer_control.py"
-    time_to_crawl = {0: 0, 9: 0, 11: 0, 13: 0, 14: 1, 17: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
-    previous = 14
+    time_to_crawl = {0: 0, 9: 0, 11: 0, 13: 0, 14: 0, 17: 0, 19: 1, 20: 0, 21: 0, 22: 0, 23: 0}
+    previous = 19
     while 1:
         now = datetime.datetime.now()
         if time_to_crawl.get(now.hour) == 0:
             os.system(cmd)
+            time.sleep(1000)
             if previous != -1:
                 time_to_crawl[previous] = 0
             previous = now.hour
-            time.sleep(2000)  # 预留10分钟用于爬虫时间
+            time_to_crawl[now.hour] = 1
+            time.sleep(1000)  # 预留10分钟用于爬虫时间
         # print(now.second)
         time.sleep(1)
 
